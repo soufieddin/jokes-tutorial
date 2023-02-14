1a8937836fb79079801c5f8523b2aa8ed8339b15
soufieddin <eddin2209@gmail.com>
2023-02-14 13:32:05 +0100

    Added action

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,6 +1,24 @@
-import { Form } from "@remix-run/react";
+import type { ActionFunction} from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { Form, useTransition } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+export let action: ActionFunction = async ({request}) => {
+  let formData = await request.formData();
+  let name = formData.get('name');
+  let content= formData.get('content')
+  if (typeof name !== 'string' || typeof content !== 'string') {
+    throw new Error('Form submitted incorrectly!')
+  }
+  let joke = await db.joke.create({
+    data: {name, content}
+  })
+  return redirect(`/jokes/${joke.id}`)
+}
 
 export default function NewJokeRoute() {
+  const navigation = useTransition();
+  const isSubmitting = navigation.state === 'submitting';
     return (
       <div>
         <p>Add your own hilarious joke</p>
@@ -16,8 +34,8 @@ export default function NewJokeRoute() {
             </label>
           </div>
           <div>
-            <button type="submit" className="button">
-              Add
+            <button disabled={isSubmitting} type="submit" className="button">
+              {isSubmitting ? "Adding..." : "Add" }
             </button>
           </div>
         </Form>
